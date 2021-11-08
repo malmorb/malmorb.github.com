3ded377249f9410b4a3c84f8fb60abcc0acc7dc4
Olle Jonsson <olle.jonsson@auctionet.com>
2021-11-08 15:38:39 +0100

    build: Attempt to use relative_assets

--- config.rb
+++ config.rb
@@ -67,7 +67,7 @@ configure :build do
   # activate :cache_buster
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   activate :asset_host
   set :asset_host, "https://malmorb.github.io"
